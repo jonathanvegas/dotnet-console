f319c238ce03eb2cc7fa19b620afbf7b45e14459
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-30 11:40:40 -0400

    Object handle

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+
+public class BankAccount {
+
+  public string AccountNumber {get; set;}
+  public decimal Balance {get; set;} = 0;
+
+
+  public BankAccount() {
+    AccountNumber = "";
+    Balance = 100;
+  }
+
+  public BankAccount(decimal newBalance){
+    AccountNumber = "";
+    Balance = newBalance;
+  }
+
+  public void MakeDeposit(decimal amount){
+    if(amount <= 0) {
+      throw new Exception("Doph!");
+    }
+    Balance += amount;
+  }
+
+  public decimal MakeDepositGetBalance(decimal amount){
+    MakeDeposit(amount);
+    return Balance;
+  }
+
+}
